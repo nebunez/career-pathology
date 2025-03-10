9ffc2065480a7ed23737de408f9b285954085e8b
nebunez <nebunez@w0rmh0le.net>
2025-03-10 00:17:37 -0400

    Update main menu

---
+++ fs-hof-2025---team-7/main_menu.gd
@@ -0,0 +1,67 @@
+class_name MainMenu
+extends Control
+
+const STORY_RES_BASE_PATH := "res://story"
+
+var _copies := {
+	art_path = "", gaming_path = "", music_path = "", business_path = "", writing_path = ""
+}
+
+@onready var _start_game: Button = %StartGame
+@onready var _exposition: Label = %Exposition
+
+# Overrides
+########################################
+
+
+func _init() -> void:
+	_copies.art_path = _get_text_file_body(STORY_RES_BASE_PATH.path_join("Art Path Copy.txt"))
+	_copies.gaming_path = _get_text_file_body(STORY_RES_BASE_PATH.path_join("Gaming Path Copy.txt"))
+	_copies.music_path = _get_text_file_body(STORY_RES_BASE_PATH.path_join("Music Path Copy.txt"))
+	_copies.business_path = _get_text_file_body(
+		STORY_RES_BASE_PATH.path_join("Business Path Copy.txt")
+	)
+	_copies.writing_path = _get_text_file_body(
+		STORY_RES_BASE_PATH.path_join("Writing Path Copy.txt")
+	)
+
+
+# Methods
+########################################
+
+
+func _get_text_file_body(filepath: String) -> String:
+	var file := FileAccess.open(filepath, FileAccess.READ)
+	var body = file.get_as_text()
+	file.close()
+
+	return body
+
+
+# Signal Connections
+####################
+
+
+func _on_art_path_button_up() -> void:
+	_exposition.text = _copies.art_path
+	_start_game.disabled = false
+
+
+func _on_gaming_path_button_up() -> void:
+	_exposition.text = _copies.gaming_path
+	_start_game.disabled = false
+
+
+func _on_music_path_button_up() -> void:
+	_exposition.text = _copies.music_path
+	_start_game.disabled = false
+
+
+func _on_business_path_button_up() -> void:
+	_exposition.text = _copies.business_path
+	_start_game.disabled = false
+
+
+func _on_writing_path_button_up() -> void:
+	_exposition.text = _copies.writing_path
+	_start_game.disabled = false
--- fs-hof-2025---team-7/menu.gd
+++ fs-hof-2025---team-7/menu.gd
@@ -4,12 +4,13 @@ extends Control
 @onready var _credits: Control = %Credits
 
 
-func _on_show_credits_button_up():
+func _on_show_credits_button_up() -> void:
 	_credits.visible = true
 
 
-func _on_credits_back_button_up():
+func _on_credits_back_button_up() -> void:
 	_credits.visible = false
 
-func _on_quit_button_up():
+
+func _on_quit_button_up() -> void:
 	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
