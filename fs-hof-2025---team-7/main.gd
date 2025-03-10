5244472594456532455ee48b3fa430fb60bee862
nebunez <nebunez@w0rmh0le.net>
2025-03-10 00:50:30 -0400

    Main Menu starts game now

--- fs-hof-2025---team-7/main.gd
+++ fs-hof-2025---team-7/main.gd
@@ -1,2 +1,26 @@
 class_name Main
 extends Node
+
+@export var game_scene: PackedScene
+
+@onready var _menu: Control = %Menu
+
+# Overrides
+########################################
+
+
+func _ready() -> void:
+	EventBus.start_game.connect(_on_start_game)
+
+
+# Methods
+########################################
+
+# Signal Connections
+####################
+
+
+func _on_start_game() -> void:
+	var game = game_scene.instantiate()
+	self.add_child(game)
+	_menu.visible = false
--- fs-hof-2025---team-7/menu.gd
+++ fs-hof-2025---team-7/menu.gd
@@ -12,5 +12,9 @@ func _on_credits_back_button_up() -> void:
 	_credits.visible = false
 
 
+func _on_start_game_button_up() -> void:
+	EventBus.start_game.emit()
+
+
 func _on_quit_button_up() -> void:
 	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
